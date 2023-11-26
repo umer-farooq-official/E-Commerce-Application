6c6ed4784c18e9b2ec2dab8f045009572c37428c
Iqra Rafiq <iqrarafiq1133@gmail.com>
2023-11-26 23:20:17 +0500

    add about and contact pages

---
+++ project/frontend/src/app/coming-soon/coming-soon.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-coming-soon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './coming-soon.component.html',
+  styleUrl: './coming-soon.component.css'
+})
+export class ComingSoonComponent {
+
+}
---
+++ project/frontend/src/app/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
